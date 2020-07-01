7d373faab855cc5b9dd8ded714f395fa9e89163f
Sizwe <nkosiczwe@gmail.com>
2020-07-01 14:57:11 +0200

    Adding myProfile page.

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,11 +1,31 @@
 package com.bookstore.controller;
 
+import java.util.Locale;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.service.UserService;
+import com.bookstore.service.impl.UserSecurityService;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserSecurityService userSecurityService;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
@@ -18,14 +38,34 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/forgotPassword")
-	public String forgotPassword(Model model) {
+	public String forgotPassword( Model model) {
 		model.addAttribute("classActiveForgotPassword", true);
 		return "myAccount";
 	}
 	
 	@RequestMapping("/newUser")
-	public String newUser(Model model) {
-		model.addAttribute("classActiveNewUser", true);
-		return "myAccount";
+	public String newUser(
+			Locale locale,
+			@RequestParam("token") String token,
+			Model model) {
+		PasswordResetToken passToken = userService.getPasswordResetToken(token);
+		
+		if(passToken == null) {
+			String message = "Invalid Token.";
+			model.addAttribute("message", message);
+			return "redirect:/badrequest";
+		}
+		
+		com.bookstore.domain.User user = passToken.getUser();
+		String username = user.getUsername();
+		
+		UserDetails userDetails = userSecurityService.loadUserByUsername(username);
+		
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		model.addAttribute("classActiveEdit", true);
+		return "myProfile";
 	}
 }
