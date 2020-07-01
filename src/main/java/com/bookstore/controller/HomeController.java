c1a7c48b05cad853bea1b9a3081dc2e9ce3d4634
Sizwe <nkosiczwe@gmail.com>
2020-07-01 19:29:19 +0200

    Adding new user controller.

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.bookstore.controller;
 
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 
-import org.apache.catalina.User;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,61 +13,102 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.Role;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.service.UserService;
 import com.bookstore.service.impl.UserSecurityService;
+import com.bookstore.utility.SecurityUtility;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private UserSecurityService userSecurityService;
-	
+
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
-	
+
 	@RequestMapping("/login")
 	public String login(Model model) {
 		model.addAttribute("classActiveLogin", true);
 		return "myAccount";
 	}
-	
+
 	@RequestMapping("/forgotPassword")
-	public String forgotPassword( Model model) {
+	public String forgotPassword(Model model) {
 		model.addAttribute("classActiveForgotPassword", true);
 		return "myAccount";
 	}
-	
-	@RequestMapping("/newUser")
-	public String newUser(
-			Locale locale,
-			@RequestParam("token") String token,
-			Model model) {
-		PasswordResetToken passToken = userService.getPasswordResetToken(token);
+
+	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
+	public String newUserPost(HttpServletRequest request, @ModelAttribute("email") String userEmail,
+			@ModelAttribute("username") String username, Model model) throws Exception {
+		model.addAttribute("classActiveNewAccount", true);
+		model.addAttribute("email", userEmail);
+		model.addAttribute("username", username);
+
+		if (userService.findByUsername(username) != null) {
+			model.addAttribute("usernameExists", true);
+
+			return "myAccount";
+		}
+
+		if (userService.findByEmail(username) != null) {
+			model.addAttribute("email", true);
+
+			return "myAccount";
+		}
 		
-		if(passToken == null) {
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(userEmail);
+		
+		String password = SecurityUtility.randomPassword();
+		
+		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
+		user.setPassword(encryptedPassword);
+		
+		Role role = new Role();
+		role.setRoleId(1);
+		role.setName("ROLE_USER");
+		Set<UserRole> userRoles = new HashSet<>();
+		userRoles.add(new UserRole(user, role));
+		userService.createUser(user, userRoles);
+
+	}
+
+	@RequestMapping("/newUser")
+	public String newUser(Locale locale, @RequestParam("token") String token, Model model) {
+		PasswordResetToken passToken = userService.getPasswordResetToken(token);
+
+		if (passToken == null) {
 			String message = "Invalid Token.";
 			model.addAttribute("message", message);
 			return "redirect:/badrequest";
 		}
-		
+
 		com.bookstore.domain.User user = passToken.getUser();
 		String username = user.getUsername();
-		
+
 		UserDetails userDetails = userSecurityService.loadUserByUsername(username);
-		
-		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
-		
+
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(),
+				userDetails.getAuthorities());
+
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
+
 		model.addAttribute("classActiveEdit", true);
 		return "myProfile";
 	}
--- src/main/java/com/bookstore/repository/UserRepository.java
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -6,5 +6,7 @@ import com.bookstore.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 	User findByUsername(String username);
+	
+	User findByEmail(String email);
 
 }
