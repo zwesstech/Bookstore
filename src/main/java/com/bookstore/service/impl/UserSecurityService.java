25f95f2a297c8d34c7a26f01ea41c10de9bf20d1
Sizwe <nkosiczwe@gmail.com>
2020-07-01 09:27:33 +0200

    Adding security config.

---
+++ src/main/java/com/bookstore/service/impl/UserSecurityService.java
@@ -0,0 +1,28 @@
+package com.bookstore.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+
+import repository.UserRepository;
+
+@Service
+public class UserSecurityService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		
+		if(null == user) {
+			throw new UsernameNotFoundException("Username not found");
+		}
+		
+		return user;
+	}
+
+}
