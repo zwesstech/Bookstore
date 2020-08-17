b2aa149b2ce1c066bd3602f46edd80b9460c9813
Sizwe <nkosiczwe@gmail.com>
2020-08-17 12:54:21 +0200

    adding userShipping and userPayment.

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -1,8 +1,8 @@
 package com.bookstore.controller;
 
-
 import java.security.Principal;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookstore.domain.Book;
 import com.bookstore.domain.User;
+import com.bookstore.domain.UserShipping;
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.domain.security.Role;
 import com.bookstore.domain.security.UserRole;
@@ -36,22 +37,23 @@ import com.bookstore.service.UserService;
 import com.bookstore.service.impl.UserSecurityService;
 import com.bookstore.utility.MailConstructor;
 import com.bookstore.utility.SecurityUtility;
+import com.bookstore.utility.USConstants;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private JavaMailSender mailSender;
-	
+
 	@Autowired
 	private MailConstructor mailConstructor;
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private UserSecurityService userSecurityService;
-	
+
 	@Autowired
 	private BookService bookService;
 
@@ -65,94 +67,90 @@ public class HomeController {
 		model.addAttribute("classActiveLogin", true);
 		return "myAccount";
 	}
-	
+
 	@RequestMapping("/bookshelf")
 	public String bookshelf(Model model) {
 		List<Book> bookList = bookService.findAll();
 		model.addAttribute("bookList", bookList);
-		
+
 		return "bookshelf";
-		
+
 	}
-	
+
 	@RequestMapping("/bookDetail")
-	public String bookDetail(
-			@PathParam("id") Long id, Model model, Principal principal
-			) {
-		if(principal != null) {
+	public String bookDetail(@PathParam("id") Long id, Model model, Principal principal) {
+		if (principal != null) {
 			String username = principal.getName();
 			User user = userService.findByUsername(username);
 			model.addAttribute("user", user);
 		}
-		
+
 		Book book = bookService.findOne(id);
-		
+
 		model.addAttribute("book", book);
-		
-		List<Integer> qtyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-		
+
+		List<Integer> qtyList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
 		model.addAttribute("qtyList", qtyList);
 		model.addAttribute("qty", 1);
-		
+
 		return "bookDetail";
 	}
-	
-	
+
 	@RequestMapping("/forgetPassword")
-	public String forgetPassword(
-			HttpServletRequest request,
-			@ModelAttribute("email") String email,
-			Model model) {
+	public String forgetPassword(HttpServletRequest request, @ModelAttribute("email") String email, Model model) {
 		model.addAttribute("classActiveForgetPassword", true);
-		
+
 		User user = userService.findByEmail(email);
-		
+
 		if (user == null) {
 			model.addAttribute("emailNotExist", true);
 			return "myAccount";
 		}
-		
+
 		String password = SecurityUtility.randomPassword();
-		
+
 		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
 		user.setPassword(encryptedPassword);
-		
+
 		userService.save(user);
-		
+
 		String token = UUID.randomUUID().toString();
 		userService.createPasswordResetTokenForUser(user, token);
-		
-		String appUrl = "http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
-		
-		SimpleMailMessage newEmail = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user, password);
+
+		String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+
+		SimpleMailMessage newEmail = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user,
+				password);
 
 		mailSender.send(newEmail);
-		
+
 		model.addAttribute("forgetPasswordEmailSent", true);
-		
+
 		return "myAccount";
 	}
-	
+
 	@RequestMapping("/myProfile")
 	public String myProfile(Model model, Principal principal) {
 		User user = userService.findByUsername(principal.getName());
 		model.addAttribute("user", user);
-		
-		UserShipping userShipping = new UserShipping();
-		model.addAttribute("userShipping", userShipping);
-		
+		model.addAttribute("userPaymentList", user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+
+		 UserShipping userShipping = new UserShipping();
+		 model.addAttribute("userShipping", userShipping);
+		 
+
 		model.addAttribute("listOfCreditCards", true);
 		model.addAttribute("listOfShippingAddresses", true);
-		
+
 		List<String> stateList = USConstants.listOfUSStateCode;
-		Collection.sort(stateList);
+		Collections.sort(stateList);
 		model.addAttribute("stateList", stateList);
 		model.addAttribute("classActiveEdit", true);
-		
+
 		return "myProfile";
 	}
-	
-	
 
 	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
 	public String newUserPost(HttpServletRequest request, @ModelAttribute("email") String userEmail,
@@ -172,36 +170,37 @@ public class HomeController {
 
 			return "myAccount";
 		}
-		
+
 		User user = new User();
 		user.setUsername(username);
 		user.setEmail(userEmail);
-		
+
 		String password = SecurityUtility.randomPassword();
-		
+
 		String encryptedPassword = SecurityUtility.passwordEncoder().encode(password);
 		user.setPassword(encryptedPassword);
-		
+
 		Role role = new Role();
 		role.setRoleId(1);
 		role.setName("ROLE_USER");
 		Set<UserRole> userRoles = new HashSet<>();
 		userRoles.add(new UserRole(user, role));
 		userService.createUser(user, userRoles);
-		
+
 		String token = UUID.randomUUID().toString();
 		userService.createPasswordResetTokenForUser(user, token);
-		
-		String appUrl = "http://"+request.getServerName()+":"+request.getServerPort()+request.getContextPath();
-		
-		SimpleMailMessage email = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user, password);
+
+		String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+
+		SimpleMailMessage email = mailConstructor.constructResetTokenEmail(appUrl, request.getLocale(), token, user,
+				password);
 
 		mailSender.send(email);
-		
+
 		model.addAttribute("emailSent", "true");
-		
+
 		return "myAccount";
-		
+
 	}
 
 	@RequestMapping("/newUser")
@@ -223,7 +222,7 @@ public class HomeController {
 				userDetails.getAuthorities());
 
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
+
 		model.addAttribute("user", user);
 
 		model.addAttribute("classActiveEdit", true);
