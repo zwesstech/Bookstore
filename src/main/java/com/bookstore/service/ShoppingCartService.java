4a49980f24c68a624fbe468f1b7c066f67804794
Sizwe <nkosiczwe@gmail.com>
2020-08-19 13:22:40 +0200

    adding more services and controls.

---
+++ src/main/java/com/bookstore/service/ShoppingCartService.java
@@ -0,0 +1,8 @@
+package com.bookstore.service;
+
+import com.bookstore.domain.ShoppingCart;
+
+public interface ShoppingCartService {
+
+	ShoppingCart upadateShoppingCart(ShoppingCart shoppingCart);
+}
