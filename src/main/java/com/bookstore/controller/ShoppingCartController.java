ff950e2f3700fb46400799d821d79c1ccc14591e
Sizwe <nkosiczwe@gmail.com>
2020-09-09 08:17:25 +0200

    adding book to shoppingCart.

--- src/main/java/com/bookstore/controller/ShoppingCartController.java
+++ src/main/java/com/bookstore/controller/ShoppingCartController.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bookstore.domain.Book;
 import com.bookstore.domain.CartItem;
 import com.bookstore.domain.ShoppingCart;
 import com.bookstore.domain.User;
+import com.bookstore.service.BookService;
 import com.bookstore.service.CartItemService;
 import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserService;
@@ -28,6 +31,9 @@ public class ShoppingCartController {
 	@Autowired
 	private ShoppingCartService shoppingCartService;
 	
+	@Autowired
+	private BookService bookService;
+	
 	@RequestMapping("/cart")
 	public String shoppingCart(Model model, Principal principal) {
 		User user = userService.findByUsername(principal.getName());
@@ -41,6 +47,27 @@ public class ShoppingCartController {
 		model.addAttribute("shoppingCart", shoppingCart);
 		
 		return "shoppingCart";
+	}
+	
+	@RequestMapping("/addItem")
+	public String addItem(
+			@ModelAttribute("book") Book book,
+			@ModelAttribute("qty") String qty,
+			Model model, Principal principal
+			) {
+		User user = userService.findByUsername(principal.getName());
+		book = bookService.findOne(book.getId());
+	
+		if (Integer.parseInt(qty) > book.getInStockNumber()) {
+			model.addAttribute("notEnoughStock", true);
+			return "forward:/bookDetail?id="+book.getId();
+		
+		}
+		
+		CartItem cartItem = cartItemService.addBookToCartItem(book, user, Integer.parseInt(qty));
+		model.addAttribute("addBookSuccess", true);
+		
+		return "forward:/bookDeatail?id="+book.getId();
 	}
 
 }
